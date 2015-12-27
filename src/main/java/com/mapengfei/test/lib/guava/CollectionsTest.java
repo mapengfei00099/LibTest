15ddbcb10edf74798fef0772869cfd8c639ce581
马鹏飞 <mapengfei@imzhiliao.com>
2015-12-27 20:25:20 +0800

    新增guava用法

---
+++ src/main/java/com/mapengfei/test/lib/guava/CollectionsTest.java
@@ -0,0 +1,13 @@
+package com.mapengfei.test.lib.guava;
+
+import com.google.common.collect.ImmutableSortedSet;
+
+/**
+ * Created by mapengfei on 15/12/27.
+ */
+public class CollectionsTest {
+    public static void main(String[] args) {
+        ImmutableSortedSet.of("a", "b", "c", "a", "d", "b");
+
+    }
+}
